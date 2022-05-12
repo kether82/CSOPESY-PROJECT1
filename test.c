129d7e4fac84bb1e4f37f322dc5ef836e1d8435a
Ivan Lim <ivalim@ymail.com>
2022-05-12 15:55:05 +0800

    added file reading

--- test.c
+++ test.c
@@ -32,37 +32,41 @@ int main() {
     struct process processes[100];
     // Get file name
     printf("Enter File Name: ");
-    fgets(filename, sizeof(filename), stdin);
-
+    // fgets(filename, sizeof(filename), stdin);
+    // filename[strcspn(filename, "\n")] = 0;
+    strcpy(filename,"test.txt");
+    puts("\n");
     file = fopen(filename, "r");
 
     if (file){
+
         // read first line
+
         fgets(firstLine, 10, file);
-        while(firstLine[i] != '/0'){
+        while(firstLine[i] != 0){
             ch = firstLine[i];
 
-            if(ch == ' '){
+            if(ch == ' ' || ch == '\n'){
                 // squish number
                 params[j] = atoi(buffer);
                 j++;
-
-                strcpy(buffer,'\0');
-                i = 0;
+                memset(buffer, 0, sizeof buffer);
+                k = 0;
             } else {
                 // number not done yet
-                buffer[i] = ch;
-                i++;
+                buffer[k] = ch;
+                k++;
             }
+            i++;
         }
+        // printf("%d %d %d", params[0], params[1], params[2]);
         i = 0;
         j = 0;
         k = 0;
         // read rest of file
         while (!feof(file)){
             ch = fgetc(file);
-
-            if(ch == ' '){
+            if(ch == ' ' || ch == '\n'){
                 // squish number
                 if(k == 0){
                     processes[j].pID = atoi(buffer);
@@ -79,7 +83,7 @@ int main() {
                     j++;
                 }
                 
-                strcpy(buffer,'\0');
+                memset(buffer, 0, sizeof buffer);
                 
                 i = 0;
             } else {
@@ -88,11 +92,16 @@ int main() {
                 i++;
             }
         }
+        // last one
+        if (feof(file)) processes[j].burstTime = atoi(buffer);
+        // printf("%d %d %d",i,j,k);
         fclose(file);
     }else{
         printf("%s not found", filename);
     }
+    // printf("%d %d %d", params[IND_ALGO], params[IND_NUMPROCESS], params[IND_TIMESLICE]);
+    // for(i = 0; i < 3; i++) printf("%d %d %d \n", processes[i].pID, processes[i].arrivalTime, processes[i].burstTime);
 
-
+    // printf("%d", processes[2].burstTime);
     return 0;
 }
